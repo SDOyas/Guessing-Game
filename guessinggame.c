3fa9f07a07d6419cc8e66e06815338275ff69618
Simon Oyas <sdoyas@cpp.edu>
2022-03-01 18:54:42 -0800

    S3 Choice 1

--- guessinggame.c
+++ guessinggame.c
@@ -13,3 +13,37 @@ void menu()
     printf("To Change Max Number Press 2\n");
     printf("To Quit Press 3\n");
 }
+
+int main()
+{
+    int max = 10;
+    int choice = 0, inp, random;
+
+        while(choice != 3)
+        {
+            menu();
+            scanf("%d", &choice);
+                if(choice == 1)
+                {
+                    srand(time(0));
+                    random = (rand() %(max - 0 + 1)) + 0;
+                        while(random != inp)
+                        {
+                            printf("Guess a number between 0 and %d:", max);
+                            scanf("%d", &inp);
+                            if(inp > random)
+                            {
+                                printf("Your guess is too high!\n\n");
+                            }
+                            else if(inp < random)
+                            {
+                                printf("Your guess is too low!\n\n");
+                            }
+                            else if(inp == random)
+                            {
+                                printf("Bingo! You won. :)");
+                            }
+                        }
+                }
+        }
+}
